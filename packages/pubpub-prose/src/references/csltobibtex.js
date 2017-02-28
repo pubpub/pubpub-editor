15bd377bc2dbb7a413712a155340fea828be9291
Thariq Shihipar <trq212@gmail.com>
2017-02-27 20:28:43 -0500

    updating css to bibtex with missing code

--- packages/pubpub-prose/src/references/csltobibtex.js
+++ packages/pubpub-prose/src/references/csltobibtex.js
@@ -11,6 +11,33 @@ var varBibTeXSyntaxTokens = {
 }
 
 /**
+ * Get name from CSL
+ *
+ * @access private
+ * @method getName
+ *
+ * @param {Object} obj - CSL input
+ *
+ * @return {String} Full name
+ */
+var getName = function ( obj ) {
+  var arr = [ 'dropping-particle', 'given', 'suffix', 'non-dropping-particle', 'family' ]
+    , res = ''
+
+  for ( var i = 0; i < arr.length; i++ ) {
+    if ( obj.hasOwnProperty( arr[ i ] ) )
+      res += obj[ arr[ i ] ] + ' '
+  }
+
+  if ( res.length )
+    res = res.slice( 0, -1 )
+  else if ( res.hasOwnProperty( 'literal' ) )
+    res = obj.literal
+
+  return res
+}
+
+/**
  * Object containing HTML strings for building JSON and BibTeX. Made to match citeproc, for compatability.
  *
  * @access private
