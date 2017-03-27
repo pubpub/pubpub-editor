9b289246ebe6d18e140caff42085f3780d77a129
Travis Rich <travis.s.rich@gmail.com>
2017-03-27 15:07:24 -0400

    Autocomplete decorations technique working

--- packages/pubpub-prose/src/prosemirror-setup/plugins/mentionsPlugin.js
+++ packages/pubpub-prose/src/prosemirror-setup/plugins/mentionsPlugin.js
@@ -13,31 +13,47 @@ const mentionsPlugin = new Plugin({
       const updateMentions = this.spec.editorView.props.viewHandlers.updateMentions;
 
       if (!sel.empty) {
-        updateMentions('no mentions');
-        return { decos: DecorationSet.empty };
+        updateMentions('inactive');
+        return {decos: DecorationSet.empty, start: null, };
       }
       const doc = editorState.doc;
-      const pos = sel.$from.pos;
-      const textNode = editorState.doc.nodeAt(pos);
-      if (textNode) {
-          const textContent = textNode.text;
+      const currentPos = editorState.selection.$to;
+      const currentNode = editorState.doc.nodeAt(currentPos.pos - 1);
+      // const pos = sel.$to.pos;
+      // const textNode = editorState.doc.nodeAt(pos);
+      // if (textNode) {
+      if (currentNode && currentNode.text) {
+          // const textContent = textNode.text;
+          const currentLine = currentNode.text;
+          const nextChIndex = currentPos.parentOffset;
+          const nextCh = currentLine.length > nextChIndex ? currentLine.charAt(nextChIndex) : ' ';
+          // console.log(currentCursor);
+          const prevChars = currentLine.substring(0, currentPos.parentOffset );
+          const startIndex = Math.max(prevChars.lastIndexOf(' ') + 1, prevChars.lastIndexOf(' ') + 1);
+          const startLetter = currentLine.charAt(startIndex);
+          // console.log(prevChars.lastIndexOf(' ') + 1, currentCursor.ch);
+          const shouldMark = startLetter === '@' && (nextCh.charCodeAt(0) === 32 || nextCh.charCodeAt(0) === 160);
+          if (shouldMark) {
+            const start = currentPos.pos - currentPos.parentOffset + startIndex;
+            const end = currentPos.pos - currentPos.parentOffset + startIndex + 1;
+            // const start = sel.$from.pos;
+            // const end = sel.$from.pos + 2;
+            const decorations = [Decoration.inline(start, end,
+              {class: 'mention-marker'},
+              { inclusiveLeft: true,
+                inclusiveRight: true,
+              }
+            )];
+            const decos = DecorationSet.create(editorState.doc, decorations);
 
-          const start = sel.$from.pos;
-          const end = sel.$from.pos + 2;
-          const decorations = [Decoration.inline(start, end,
-            {class: 'selection-marker'},
-            { inclusiveLeft: true,
-              inclusiveRight: true,
-            }
-          )];
-  				const decos = DecorationSet.create(editorState.doc, decorations);
-
-          console.log(this.spec.editorView.props);
-          updateMentions('hi');
-          return {decos: decos, start, end};
+            // console.log(this.spec.editorView.props);
+            updateMentions('active');
+            return {decos: decos, start, end};
+          }
+          
       }
-      updateMentions('no mentions');
-      return {decos: DecorationSet.empty, start: sel.$from.pos, };
+      updateMentions('inactive');
+      return {decos: DecorationSet.empty, start: null, };
     }
   },
     view: function(editorView) {
@@ -63,16 +79,17 @@ const mentionsPlugin = new Plugin({
       console.log(evt.type, evt.key);
 
 
-      if (evt.type === 'keydown' && evt.key === 'ArrowUp') {
+      if (evt.type === 'keydown' && (evt.key === 'ArrowUp' || evt.key === 'ArrowDown')) {
         const sel = view.state.selection;
         if (!sel.empty) {
           return false;
         }
-        const selFrom = sel.$from.pos;
+        // const selFrom = sel.$from.pos;
         const pluginState = this.getState(view.state);
         const {start, end} = pluginState;
-        console.log('Got', start, end, selFrom);
-        if (start && selFrom >= start && selFrom <= end) {
+        // console.log('Got', start, end, selFrom);
+        // if (start && selFrom >= start && selFrom <= end) {
+        if (start) {
           return true;
         }
       }
--- packages/pubpub-prose/src/react-editors/reactRichEditor.jsx
+++ packages/pubpub-prose/src/react-editors/reactRichEditor.jsx
@@ -24,6 +24,9 @@ export const RichEditorWrapper = React.createClass({
 		return {
 			docJSON: null,
 			fileMap: null,
+			visible: undefined,
+			top: 0,
+			left: 0,
 		};
 	},
 
@@ -34,15 +37,69 @@ export const RichEditorWrapper = React.createClass({
 		this.createEditor(null);
 	},
 
-  componentWillUpdate(nextProps) {
-    if (this.props) {
+	componentWillUpdate(nextProps) {
+		if (this.props) {
 
-    }
-  },
+		}
+	},
 
-  onChange(editorState) {
+	onChange(editorState) {
 		this.props.onChange();
-  },
+		// console.log(editorState, other, other2)
+		// console.log(this.editor.view);
+		// console.log(this.editor.view.state);
+		// const currentPos = this.editor.view.state.selection.$to;
+		// console.log(currentPos.pos);
+		// const currentNode = this.editor.view.state.doc.nodeAt(currentPos.pos - 1);
+		// console.log(currentNode);
+		// if (currentNode && currentNode.text) {
+		// 	// const prevChars = currentNode.text.substring(0, currentPos.parentOffset);
+		// 	// console.log(prevChars);
+		// 	// const nextCh = currentPos.parentOffset < currentNode.text.length ? currentNode.text.charAt(currentPos.parentOffset + 1) : ' ';
+		// 	// const startIndex = prevChars.lastIndexOf(' ') + 1;
+		// 	// const startLetter = currentNode.text.charAt(startIndex);
+		// 	// const shouldMark = startLetter === '@' && nextCh === ' ';
+		// 	// console.log(shouldMark);
+		// 	const currentLine = currentNode.text;
+		// 	const nextChIndex = currentPos.parentOffset;
+		// 	const nextCh = currentLine.length > nextChIndex ? currentLine.charAt(nextChIndex) : ' ';
+		// 	// console.log(currentCursor);
+		// 	const prevChars = currentLine.substring(0, currentPos.parentOffset );
+		// 	const startIndex = Math.max(prevChars.lastIndexOf(' ') + 1, prevChars.lastIndexOf(' ') + 1);
+		// 	const startLetter = currentLine.charAt(startIndex);
+		// 	// console.log(prevChars.lastIndexOf(' ') + 1, currentCursor.ch);
+		// 	const shouldMark = startLetter === '@' && (nextCh.charCodeAt(0) === 32 || nextCh.charCodeAt(0) === 160);
+		// 	// console.log('currentLine ', JSON.stringify(currentLine));
+		// 	// console.log('currentPos.parentOffset', currentPos.parentOffset);
+		// 	// console.log('nextChIndex ', nextChIndex);
+		// 	// console.log("prevChars.lastIndexOf(' ')", prevChars.lastIndexOf(' '));
+		// 	// console.log('char code', currentLine.charCodeAt(nextChIndex))
+		// 	// console.log('currentLine.length ', currentLine.length);
+		// 	// console.log('prevChars ', JSON.stringify(prevChars));
+		// 	// console.log('startIndex ', startIndex);
+		// 	// console.log('startLetter ', JSON.stringify(startLetter));
+		// 	// console.log('nextCh ', JSON.stringify(nextCh));
+		// 	console.log(shouldMark);
+		// 	if (shouldMark) {
+		// 		const newDecorations = DecorationSet.create(this.editor.view.state.doc, [Decoration.inline(currentPos.parentOffset - 3, currentPos.parentOffset - 1)])
+		// 		this.editor.view.setProps({ decorations: newDecorations });
+		// 		// this.editor.view.markRange(currentPos.parentOffset - 3, currentPos.parentOffset - 1, {className: 'yooo'});
+		// 	}
+		// }
+		
+		// // const nextChIndex = currentCursor.ch;
+		// // const nextCh = currentLine.length > nextChIndex ? currentLine.charAt(nextChIndex) : ' ';
+		// // // console.log(currentCursor);
+		// // const prevChars = currentLine.substring(0, currentCursor.ch);
+		// // const startIndex = prevChars.lastIndexOf(' ') + 1;
+		// // const startLetter = currentLine.charAt(startIndex);
+		// // // console.log(prevChars.lastIndexOf(' ') + 1, currentCursor.ch);
+		// // const shouldMark = startLetter === '@' && nextCh === ' ' && !cm.getSelection();
+
+
+		// // debugger;
+
+	},
 
 	getMarkdown() {
 		return this.editor.toMarkdown();
@@ -75,14 +132,47 @@ export const RichEditorWrapper = React.createClass({
 	},
 
 	updateMentions(mentions) {
-		this.setState({mentions});
+		// this.setState({mentions});
+
+		if (mentions === 'hi') {
+			setTimeout(()=>{
+				const container = document.getElementById('rich-editor-container');
+				const mark = document.getElementsByClassName('mention-marker')[0];
+				// console.log(container, mark);
+				const top = mark.getBoundingClientRect().bottom - container.getBoundingClientRect().top;
+				const left = mark.getBoundingClientRect().left - container.getBoundingClientRect().left;
+				this.setState({
+					visible: true,
+					top: top,
+					left: left,
+				});
+			}, 0);
+		} else {
+			this.setState({
+				visible: false,
+			});
+		}
+	},
+
+	mentionStyle: function(top, left, visible) {
+		return {
+			zIndex: 10, 
+			position: 'absolute', 
+			left: left, 
+			top: top, 
+			opacity: visible ? 1 : 0, 
+			pointerEvents: visible ? 'auto' : 'none', 
+			transition: '.1s linear opacity'
+		};
 	},
 
 	render: function() {
 		const {mentions} = this.state;
 		return (
-			<div>
-				<div>{mentions}</div>
+			<div style={{ position: 'relative' }} id={'rich-editor-container'}>
+				<div className={'pt-card pt-elevation-4'} style={this.mentionStyle(this.state.top, this.state.left, this.state.visible)}>
+					Autocomplete me!
+				</div>
 				<div ref="container" className="pubEditor" id="pubEditor"></div>
 			</div>
 		);
