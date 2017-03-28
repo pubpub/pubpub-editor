14477e2110135b69e5657f3975837ae709ecb0ed
Travis Rich <travis.s.rich@gmail.com>
2017-03-28 17:31:38 -0400

    Cleanup storybook files a bit

--- packages/pubpub-prose/stories/index.js
+++ packages/pubpub-prose/stories/index.js
@@ -1,35 +1,28 @@
-import {SingleMention, SingleReference} from './sampledocs';
-import { action, storiesOf } from '@kadira/storybook';
-
-import Autocomplete from './storybookAutocomplete';
 import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+
 import RichEditor from './storybookRichEditor';
 import MarkdownEditor from './storybookMarkdownEditor';
 import FullEditor from './storybookFullEditor';
+import { SingleMention, SingleReference } from './sampledocs';
 
 storiesOf('Rich Editor', module)
-	.add('basic ', () => (
-		<RichEditor/>
-	))
-	.add('single reference', () => (
-		<RichEditor initialState={SingleReference}/>
-	))
-	.add('single mention', () => (
-		<RichEditor initialState={SingleMention}/>
-	));
-	;
+.add('basic ', () => (
+	<RichEditor />
+))
+.add('single reference', () => (
+	<RichEditor initialState={SingleReference} />
+))
+.add('single mention', () => (
+	<RichEditor initialState={SingleMention} />
+));
 
-	storiesOf('Markdown Editor', module)
-	.add('basic ', () => (
-		<MarkdownEditor/>
-	));
+storiesOf('Markdown Editor', module)
+.add('basic ', () => (
+	<MarkdownEditor />
+));
 
-	storiesOf('Full Editor', module)
-	.add('basic ', () => (
-		<FullEditor/>
-	));
-
-	// storiesOf('Autocomplete', module)
-	// .add('basic ', () => (
-	// 	<Autocomplete/>
-	// ));
+storiesOf('Full Editor', module)
+.add('basic ', () => (
+	<FullEditor />
+));
