4d3aa35fab33d04ad0a42334035fdba21ff97916
Thariq Shihipar <trq212@gmail.com>
2017-04-03 18:43:40 -0400

    safely checking citations content

--- packages/pubpub-prose/dist/markdown/markdownToJson.js
+++ packages/pubpub-prose/dist/markdown/markdownToJson.js
@@ -15,35 +15,37 @@ var markdownToJSON = function markdownToJSON(markdown, citationsList) {
 
   if (citationsList && citationsList.length > 0) {
     var citations = doc.content[1];
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
+    if (citations && citations.content && citations.content.length > 0) {
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
 
-    try {
-      for (var _iterator = citations.content[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        var citation = _step.value;
-
-        if (citation.attrs.citationID) {
-          (function () {
-            var citationID = citation.attrs.citationID;
-            var citationData = citationsList.find(function (elem) {
-              return elem.id === citationID;
-            });
-            citation.attrs.data = citationData;
-          })();
-        }
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
       try {
-        if (!_iteratorNormalCompletion && _iterator.return) {
-          _iterator.return();
+        for (var _iterator = citations.content[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var citation = _step.value;
+
+          if (citation.attrs.citationID) {
+            (function () {
+              var citationID = citation.attrs.citationID;
+              var citationData = citationsList.find(function (elem) {
+                return elem.id === citationID;
+              });
+              citation.attrs.data = citationData;
+            })();
+          }
         }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
       } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
         }
       }
     }
--- packages/pubpub-prose/src/markdown/markdownToJson.js
+++ packages/pubpub-prose/src/markdown/markdownToJson.js
@@ -11,13 +11,15 @@ const markdownToJSON = (markdown, citationsList) => {
 
   if (citationsList && citationsList.length > 0) {
     const citations = doc.content[1];
-    for (const citation of citations.content) {
-      if (citation.attrs.citationID) {
-        const citationID = citation.attrs.citationID;
-        const citationData = citationsList.find(function(elem) {
-          return (elem.id === citationID);
-        });
-        citation.attrs.data = citationData;
+    if (citations && citations.content && citations.content.length > 0) {
+      for (const citation of citations.content) {
+        if (citation.attrs.citationID) {
+          const citationID = citation.attrs.citationID;
+          const citationData = citationsList.find(function(elem) {
+            return (elem.id === citationID);
+          });
+          citation.attrs.data = citationData;
+        }
       }
     }
   }
