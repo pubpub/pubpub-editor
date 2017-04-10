a54f74e57abcfe378374b4d58c1b16f217475b8f
Thariq Shihipar <trq212@gmail.com>
2017-04-10 12:45:18 -0400

    adding export of csltoBibtex

--- packages/pubpub-prose/src/index.js
+++ packages/pubpub-prose/src/index.js
@@ -9,4 +9,4 @@ export { jsonToMarkdown } from './markdown/jsonToMarkdown';
 export { markdownToJSON } from './markdown/markdownToJson';
 
 export bibtexToCSL from './references/bibtextocsl';
-
+export { csltoBibtex } from './references/csltobibtex';
