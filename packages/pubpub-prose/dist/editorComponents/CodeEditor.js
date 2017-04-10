0dfdbcc10b9ae5c769c546f0bd4938735c6d6a73
Thariq Shihipar <trq212@gmail.com>
2017-04-10 12:29:46 -0400

    compiling code editor

---
+++ packages/pubpub-prose/dist/editorComponents/CodeEditor.js
@@ -0,0 +1,69 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.CodeEditor = undefined;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _codemirror = require('codemirror');
+
+var _codemirror2 = _interopRequireDefault(_codemirror);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+require('../../style/code.scss');
+
+var styles = void 0;
+var CodeEditor = exports.CodeEditor = _react2.default.createClass({
+	displayName: 'CodeEditor',
+
+	propTypes: {
+		initialContent: _react.PropTypes.string,
+		onChange: _react.PropTypes.func
+	},
+
+	codeMirror: undefined,
+
+	componentDidMount: function componentDidMount() {
+		var _this = this;
+
+		var element = document.getElementById('myCodeEditor');
+		if (element) {
+			this.codeMirror = _codemirror2.default.fromTextArea(element, {
+				autofocus: true,
+				value: this.props.initialContent || ''
+			});
+
+			if (this.props.onChange) {
+				this.codeMirror.on('change', function (cm) {
+					_this.props.onChange(cm.doc.getValue());
+				});
+			}
+		}
+	},
+	componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+		if (this.props.initialContent !== nextProps.initialContent) {
+			this.codeMirror.value(nextProps.initialContent);
+		}
+	},
+	render: function render() {
+		return _react2.default.createElement(
+			'div',
+			{ id: 'code-editor-container', style: styles.container },
+			_react2.default.createElement('textarea', { id: 'myCodeEditor' })
+		);
+	}
+});
+
+exports.default = CodeEditor;
+
+
+styles = {
+	container: {
+		position: 'relative'
+	}
+};
