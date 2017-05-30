80e3bf91d82469b1387533a8f2d07f0e5897cd9c
Thariq Shihipar <trq212@gmail.com>
2017-05-30 12:21:20 -0400

    fixing reference not found bug

--- packages/pubpub-editor/src/references/bibtextocsl.js
+++ packages/pubpub-editor/src/references/bibtextocsl.js
@@ -10,13 +10,24 @@ const parseBibTeX = function(bibString) {
             }
         }
     )
-    let bibDB = parser.output
+    let bibDB = parser.output;
+
     if (parser.errors.length) {
       console.log(parser.errors)
       return null;
     }
+    const refKeys = [];
+
+    for (const bibJSON of Object.values(bibDB)) {
+      refKeys.push(bibJSON.entry_key);
+    }
+
     let exporter = new CSLExporter(bibDB);
-    return Object.values(exporter.output);
+    const bibData = Object.values(exporter.output);
+    for (const [index, bibEntry] of bibData.entries()) {
+      bibEntry.id = refKeys[index];
+    }
+    return bibData;
 }
 
 export default parseBibTeX;
