89d4f87b01aff85b6fd69fda7aa849152aaef7ef
Travis Rich <travis.s.rich@gmail.com>
2017-10-03 20:13:42 -0400

    Cleaning up imageAddon files and updating ImageStatic

--- src/addons/Image/ImageStatic.js
+++ src/addons/Image/ImageStatic.js
@@ -1,85 +1,47 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import PropTypes from 'prop-types';
 
-let styles = {};
+require('./imageAddon.scss');
 
 const propTypes = {
+	caption: PropTypes.string,
 	url: PropTypes.string,
-	align: PropTypes.oneOf(['inline', 'full', 'left', 'right', 'inline-word', 'max']),
-	size: PropTypes.string,
+	align: PropTypes.oneOf(['full', 'left', 'right', 'center']),
+	size: PropTypes.number, // Number as percentage width
 };
 
-class ImageStatic extends Component {
-
-	render() {
-		const { size, align, url, children, caption, selected } = this.props;
-		const captionNode = (children) ? children[0] : null;
-		const captionText = (captionNode) ? captionNode.text : '';
-
-		return (
-			<div ref="embedroot" className={'pub-embed ' + (this.props.className) ? this.props.className : null }>
-				<figure style={styles.figure({size, align, selected: false})}>
-					<div style={styles.row({size, align})}>
-						<img style={styles.image} src={url}/>
-					</div>
-					<figcaption style={styles.caption({size, align})}>
-						<div style={styles.captionInput} ref="captioninsert">
-							{caption}
-						</div>
-					</figcaption>
-				</figure>
-			</div>
-		);
-	}
-}
-
-styles = {
-	row: function ({ size, align }) {
-		return {
-			width: (align !== 'max') ? size : '100%',
-			position: 'relative',
-			display: 'table-row'
-		};
-	},
-	captionInput: {
-		width: '100%',
-		border: 'none',
-		fontSize: '1em',
-		minHeight: '1em',
-	},
-	image: {
-		width: '100%',
-	},
-	figure: function({size, align, selected}) {
-		const style = {
-			width: size,
-			display: 'table',
-			outline: (selected) ? '3px solid #BBBDC0' : '3px solid transparent',
-			transition: 'outline-color 0.15s ease-in',
-			paddingTop: '10px',
-			marginRight: (align === 'left') ? '20px' : null,
-			marginLeft: (align === 'right') ? '20px' : null,
-		};
-		if (align === 'max') {
-			style.width = 'calc(100% + 30px)';
-			style.margin = '0 0 0 -15px';
-		} else if (align === 'left') {
-			style.float = 'left';
-		} else if (align === 'right') {
-			style.float = 'right';
-		} else if (align === 'full') {
-			style.margin = '0 auto';
-		}
-		return style;
-	},
-	caption: function({size, align}) {
-		const style = {
-			width: size,
-			display: 'table-row',
-		};
-		return style;
-	},
+const defaultProps = {
+	caption: '',
+	url: '',
+	align: 'center',
+	size: 50,
 };
 
+const ImageStatic = function(props) {
+	const figFloat = props.align === 'left' || props.align === 'right' ? props.align : 'none';
+	const figMargin = props.align === 'left' || props.align === 'right' ? '10px' : '0em auto 1em';
+	const figWidth = props.align === 'full' ? '100%' : `${props.size}%`;
+	const figStyle = {
+		width: figWidth,
+		margin: figMargin,
+		float: figFloat,
+	};
+
+	return (
+		<div className={'figure-wrapper'}>
+			<figure className={'image'} style={figStyle}>
+				<img
+					src={props.url}
+					alt={props.caption}
+				/>
+				<figcaption>
+					{props.caption}
+				</figcaption>
+			</figure>
+		</div>
+	);
+};
+
+ImageStatic.propTypes = propTypes;
+ImageStatic.defaultProps = defaultProps;
 export default ImageStatic;
