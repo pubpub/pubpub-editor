c1f446ee77958c6063d512c48a2377c4bbd76333
Travis Rich <travis.s.rich@gmail.com>
2017-10-28 10:13:40 -0500

    Cleanup comments

--- src/addons/HighlightMenu/HighlightMenu.js
+++ src/addons/HighlightMenu/HighlightMenu.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Plugin, TextSelection } from 'prosemirror-state';
 import { Decoration, DecorationSet } from 'prosemirror-view';
-// import { Slice, Fragment } from 'prosemirror-model';
 import * as textQuote from 'dom-anchor-text-quote';
 import stringHash from 'string-hash';
 import { Popover, PopoverInteractionKind, Position } from '@blueprintjs/core';
@@ -54,101 +53,40 @@ class HighlightMenu extends Component {
 						const datafrom = newData.from;
 						const datato = newData.to;
 						posBasedExact = editorState.doc.textBetween(datafrom, datato);
-						// editorState.doc.slice(datafrom, datato).content.forEach((sliceNode)=>{ posBasedExact += sliceNode.textContent; });
 						stillInTact = posBasedExact === newData.exact;
 					}
-					// debugger;
-					// console.log(posBasedExact, stillInTact);
 					if (transaction.meta.clearTempSelection) {
 						const tempSelections = decoSet.find().filter((item)=>{ return item.type.attrs.class.indexOf('temp-selection') > -1; });
 						newDecoSet = decoSet.remove(tempSelections);
-					// } else if (transaction.meta.newSelection) {
-					// 	const from = transaction.meta.newSelectionData.from;
-					// 	const to = transaction.meta.newSelectionData.to;
-					// 	let exact = '';
-					// 	editorState.doc.slice(from, to).content.forEach((sliceNode)=>{ exact += sliceNode.textContent; });
-					// 	console.log('exact is', exact, 'transactionexact is', transaction.meta.newSelectionData.exact);
-					// 	if (exact === transaction.meta.newSelectionData.exact) {
-					// 		newDecoSet = decoSet.add(editorState.doc, [Decoration.inline(from, to, {
-					// 			class: `cite-deco ${transaction.meta.newSelectionData.id}`,
-					// 		})]);
-					// 	} else {
-					// 		const container = document.getElementsByClassName(primaryEditorClassName)[0];
-					// 		const range = textQuote.toRange(container, {
-					// 			exact: transaction.meta.newSelectionData.exact,
-					// 			prefix: transaction.meta.newSelectionData.prefix,
-					// 			suffix: transaction.meta.newSelectionData.suffix,
-					// 		});
-					// 		if (!range) {
-					// 			newDecoSet = decoSet;
-					// 		} else {
-					// 			const from = editorState.doc.resolve(range.commonAncestorContainer.pmViewDesc.posAtStart + range.startOffset).pos;
-					// 			const to = editorState.doc.resolve(range.commonAncestorContainer.pmViewDesc.posAtStart + range.endOffset).pos;
-					// 			// const from = editorState.doc.resolve(range.endContainer.pmViewDesc.posAtStart - range.startContainer.length).pos;
-					// 			// const to = editorState.doc.resolve(range.endContainer.pmViewDesc.posAtStart).pos;
-					// 			// debugger;
-					// 			newDecoSet = decoSet.add(editorState.doc, [Decoration.inline(from, to, {
-					// 				class: `cite-deco ${transaction.meta.newSelectionData.id}`,
-					// 			})]);
-					// 		}
-					// 	}
-					// }
-
-
-
 					} else if (transaction.meta.newSelection && (transaction.meta.newSelectionData.version || stillInTact)) {
-						// console.log('is permanent', transaction.meta.newSelectionData.permanent);
 						const from = transaction.meta.newSelectionData.from;
 						const to = transaction.meta.newSelectionData.to;
 						const tempSelections = decoSet.find().filter((item)=>{ return item.type.attrs.class.indexOf('temp-selection') > -1; });
-						const classString = `cite-deco ${transaction.meta.newSelectionData.id} ${transaction.meta.newSelectionData.permanent ? 'permanent' : ''}`.trim();
-						// console.log('classString', classString)
 						newDecoSet = decoSet.remove(tempSelections).add(editorState.doc, [Decoration.inline(from, to, {
-							class: classString,
+							class: `cite-deco ${transaction.meta.newSelectionData.id} ${transaction.meta.newSelectionData.permanent ? 'permanent' : ''}`.trim(),
 						})]);
 					} else if (transaction.meta.newSelection && !transaction.meta.newSelectionData.version) {
-						// console.log('is permanent', transaction.meta.newSelectionData.permanent);
 						const container = document.getElementsByClassName(primaryEditorClassName)[0];
 						const range = textQuote.toRange(container, {
 							exact: transaction.meta.newSelectionData.exact,
 							prefix: transaction.meta.newSelectionData.prefix,
 							suffix: transaction.meta.newSelectionData.suffix,
 						});
-						// console.log(transaction.meta.newSelectionData);
-						// console.log('Doing it this way', range);
-						// debugger;
-
-						if (!range.startContainer.pmViewDesc || !range.endContainer.pmViewDesc) {
-							console.log('Couldnt get pmview');
-							debugger;
-						}
 						let resolvedStartContainer = range.startContainer;
 						while (!resolvedStartContainer.pmViewDesc && resolvedStartContainer.className !== 'ProseMirror' && resolvedStartContainer !== null) {
-							console.log('start', transaction.meta.newSelectionData.exact);
 							resolvedStartContainer = resolvedStartContainer.parentElement;
 						}
 						let resolvedEndContainer = range.endContainer;
 						while (!resolvedEndContainer.pmViewDesc && resolvedEndContainer.className !== 'ProseMirror' && resolvedEndContainer !== null) {
-							console.log('end', transaction.meta.newSelectionData.exact);
 							resolvedEndContainer = resolvedEndContainer.parentElement;
-						}
-
-						if (!resolvedStartContainer.pmViewDesc || !resolvedEndContainer.pmViewDesc) {
-							console.log('Couldnt get pmview');
-							debugger;
 						}
 						if (!range || !resolvedStartContainer.pmViewDesc || !resolvedEndContainer.pmViewDesc) {
 							newDecoSet = decoSet;
 						} else {
 							const from = editorState.doc.resolve(resolvedStartContainer.pmViewDesc.posAtStart + range.startOffset).pos;
 							const to = editorState.doc.resolve(resolvedEndContainer.pmViewDesc.posAtStart + range.endOffset).pos;
-							// const from = editorState.doc.resolve(range.endContainer.pmViewDesc.posAtStart - range.startContainer.length).pos;
-							// const to = editorState.doc.resolve(range.endContainer.pmViewDesc.posAtStart).pos;
-							// debugger;
-							const classString = `cite-deco ${transaction.meta.newSelectionData.id} ${transaction.meta.newSelectionData.permanent ? 'permanent' : ''}`.trim();
-							// console.log('classString', classString)
 							newDecoSet = decoSet.add(editorState.doc, [Decoration.inline(from, to, {
-								class: classString,
+								class: `cite-deco ${transaction.meta.newSelectionData.id} ${transaction.meta.newSelectionData.permanent ? 'permanent' : ''}`.trim(),
 							})]);
 						}
 					} else {
@@ -171,15 +109,12 @@ class HighlightMenu extends Component {
 			top: null,
 			to: undefined,
 			from: undefined,
-			hash: undefined,
 			activeHover: undefined,
 			createdHighlights: [],
 		};
 		this.onChange = this.onChange.bind(this);
 		this.handleMouseDown = this.handleMouseDown.bind(this);
-		// this.handleClick = this.handleClick.bind(this);
 		this.completeNewDiscussion = this.completeNewDiscussion.bind(this);
-		// this.cancelNewDiscussion = this.cancelNewDiscussion.bind(this);
 		this.handleNewDiscussion = this.handleNewDiscussion.bind(this);
 		this.handleMouseEnter = this.handleMouseEnter.bind(this);
 		this.handleMouseLeave = this.handleMouseLeave.bind(this);
@@ -210,7 +145,6 @@ class HighlightMenu extends Component {
 					permanent: item.permanent
 				});
 			}, 1);
-			
 		});
 	}
 
@@ -224,21 +158,13 @@ class HighlightMenu extends Component {
 			const currentFromPos = view.state.selection.$from.pos;
 			const currentToPos = view.state.selection.$to.pos;
 			const inlineTop = view.coordsAtPos(currentFromPos).top - container.getBoundingClientRect().top;
-			// const sel = view.state.selection;
 			const exact = editorState.doc.textBetween(currentFromPos, currentToPos);
-			// let string = '';
-			// sel.content().content.forEach((node)=>{ string += node.textContent; });
 			const prefix = editorState.doc.textBetween(Math.max(0, currentFromPos - 10), Math.max(0, currentFromPos));
 			const suffix = editorState.doc.textBetween(Math.min(editorState.doc.nodeSize - 2, currentToPos), Math.min(editorState.doc.nodeSize - 2, currentToPos + 10));
-			// let prefix = '';
-			// editorState.doc.slice(Math.max(0, currentFromPos - 10), Math.max(0, currentFromPos)).content.forEach((node)=>{ prefix += node.textContent; });
-			// let suffix = '';
-			// editorState.doc.slice(Math.min(editorState.doc.nodeSize - 2, currentToPos), Math.min(editorState.doc.nodeSize - 2, currentToPos + 10)).content.forEach((node)=>{ suffix += node.textContent; });
 			const output = {
 				top: inlineTop,
 				to: currentToPos,
 				from: currentFromPos,
-				// hash: stringHash(exact),
 				exact: exact,
 				prefix: prefix,
 				suffix: suffix,
@@ -256,28 +182,12 @@ class HighlightMenu extends Component {
 		evt.preventDefault();
 	}
 
-	// handleClick() {
-	// 	console.log('Clicked it');
-	// 	this.props.view.focus();
-
-	// 	const transaction = this.props.view.state.tr;
-	// 	transaction.setMeta('newSelection', true);
-		
-	// 	const possible = 'abcdefghijklmnopqrstuvwxyz';
-	// 	let hash = '';
-	// 	for (let index = 0; index < 8; index++) {
-	// 		hash += possible.charAt(Math.floor(Math.random() * possible.length));
-	// 	}
-	// 	transaction.setMeta('newSelectionId', hash);
-	// 	this.props.view.dispatch(transaction);
-	// }
 	handleNewDiscussion() {
 		const clearSelectionTransaction = this.props.view.state.tr.setSelection(new TextSelection(this.props.view.state.selection.$to));
 		this.props.view.dispatch(clearSelectionTransaction);
 		this.props.onNewDiscussion({
 			from: this.state.from,
 			to: this.state.to,
-			hash: this.props.versionId ? undefined : this.state.hash,
 			version: this.props.versionId,
 			exact: this.state.exact,
 			prefix: this.state.prefix,
@@ -285,34 +195,14 @@ class HighlightMenu extends Component {
 			// onComplete: this.completeNewDiscussion,
 			// onCancel: this.cancelNewDiscussion,
 		});
-		// const transaction = this.props.view.state.tr;
-		// transaction.setMeta('newSelection', true);
-		// transaction.setMeta('newSelectionData', {
-		// 	from: this.state.from,
-		// 	to: this.state.to,
-		// 	id: 'temp-selection',
-		// });
-		// this.props.view.dispatch(transaction);
-		// setTimeout(()=> {
-		// 	this.completeNewDiscussion({
-		// 		from: this.state.from,
-		// 		to: this.state.to,
-		// 		id: 'fakeid',
-		// 		version: this.props.versionId,
-		// 		exact: this.state.exact,
-		// 		prefix: this.state.prefix,
-		// 		suffix: this.state.suffix,
-		// 	});
-		// }, 1000);
 	}
-	completeNewDiscussion({ from, to, id, hash, version, exact, prefix, suffix, permanent }) {
+	completeNewDiscussion({ from, to, id, version, exact, prefix, suffix, permanent }) {
 		const transaction = this.props.view.state.tr;
 		transaction.setMeta('newSelection', true);
 		transaction.setMeta('newSelectionData', {
 			from: from,
 			to: to,
 			id: id,
-			hash: hash,
 			version: version,
 			exact: exact,
 			prefix: prefix,
@@ -345,9 +235,6 @@ class HighlightMenu extends Component {
 			top: 0,
 			right: -20,
 		};
-		/* I think this will cause a problem if a single window has more */
-		/* than one HighlightMenu in the globalscope */
-		/* Don't know the right way to get pluginKey here */
 		const pluginKeyString = this.props.editorState.plugins.reduce((prev, curr)=> {
 			if (curr.key.indexOf('HighlightMenu') !== -1) {
 				return curr.key;
@@ -363,7 +250,6 @@ class HighlightMenu extends Component {
 				const className = item.type.attrs.class.replace('cite-deco ', '').replace(' permanent', '');
 				const isPermanent = item.type.attrs.class.indexOf('permanent') > -1;
 				const elem = document.getElementsByClassName(className)[0];
-				// const wrapper = document.getElementsByClassName(this.props.primaryEditorClassName)[0];
 				const wrapper = document.getElementsByClassName('things-wrapper')[0];
 				const output = elem
 					? {
