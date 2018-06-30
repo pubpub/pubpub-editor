45c2a4121c9b9c53cbc12965634a88facd4450ef
Travis Rich <travis.s.rich@gmail.com>
2018-06-30 10:09:31 -0400

    Update Latex Addon

--- src/addons/Latex/LatexEditable.js
+++ src/addons/Latex/LatexEditable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Portal } from 'react-portal';
 import { NodeSelection } from 'prosemirror-state';
 
 require('./latex.scss');
@@ -13,6 +14,8 @@ const propTypes = {
 	view: PropTypes.object.isRequired,
 	updateAttrs: PropTypes.func.isRequired,
 	changeNode: PropTypes.func.isRequired,
+	onOptionsRender: PropTypes.func.isRequired,
+	optionsContainerRef: PropTypes.object.isRequired,
 };
 
 class LatexEditable extends Component {
@@ -24,6 +27,7 @@ class LatexEditable extends Component {
 		this.changeToInline = this.changeToInline.bind(this);
 		this.changeToBlock = this.changeToBlock.bind(this);
 		this.refocusNode = this.refocusNode.bind(this);
+		this.portalRefFunc = this.portalRefFunc.bind(this);
 	}
 
 	componentWillReceiveProps(nextProps) {
@@ -51,6 +55,7 @@ class LatexEditable extends Component {
 			this.refocusNode(1);
 		}
 	}
+
 	handleValueChange(evt) {
 		this.props.updateAttrs({ value: evt.target.value, html: this.state.html });
 		if (!this.props.isBlock) {
@@ -58,6 +63,7 @@ class LatexEditable extends Component {
 		}
 		this.props.renderFunction(evt.target.value, this.props.isBlock, this.handleHTMLChange);
 	}
+
 	handleHTMLChange(html) {
 		this.setState({ html: html });
 		/* This is a bit funky, but to avoid losing cursor position on */
@@ -66,6 +72,7 @@ class LatexEditable extends Component {
 		/* On deselect of the node, we fire an updateAttrs to make sure */
 		/* that last value is synced to attrs. */
 	}
+
 	refocusNode(offset = 0) {
 		const view = this.props.view;
 		const pos = view.state.selection.from + offset;
@@ -74,42 +81,64 @@ class LatexEditable extends Component {
 		view.dispatch(transaction);
 	}
 
+	portalRefFunc(elem) {
+		/* Used to call onOptioneRender so that optionsBox can be placed */
+		if (elem) {
+			const domAtPos = this.props.view.domAtPos(this.props.view.state.selection.from);
+			const nodeDom = domAtPos.node.childNodes[domAtPos.offset];
+			this.props.onOptionsRender(nodeDom, this.props.optionsContainerRef.current);
+		}
+	}
+
 	render() {
 		return (
-			<div className={`latex-wrapper ${this.props.isBlock ? 'block' : ''} ${this.props.isSelected ? 'selected' : ''}`}>
-				{this.props.isSelected &&
-					<style>{`
-						.latex-wrapper { vertical-align: top !important; }
-					`}</style>
-				}
-				<div className={'render-wrapper'}>
+			<div className={`latex-wrapper ${this.props.isBlock ? 'block' : ''}`}>
+				<div className={`render-wrapper ${this.props.isSelected ? 'isSelected' : ''}`}>
 					<span
 						className={'editable-render'}
 						dangerouslySetInnerHTML={{ __html: this.state.html }}
 					/>
-					{this.props.isSelected &&
-						<div className={'options-wrapper'}>
-							<div className={'top-row'}>
-								<div className={'pt-button-group pt-minimal'}>
-									<button
-										className={`pt-button pt-icon-align-left ${!this.props.isBlock ? 'pt-active' : ''}`}
-										onClick={this.changeToInline}
-									/>
-									<button
-										className={`pt-button pt-icon-align-justify ${this.props.isBlock ? 'pt-active' : ''}`}
-										onClick={this.changeToBlock}
-									/>
-								</div>
-							</div>
+				</div>
+				{this.props.isSelected &&
+					<Portal 
+						ref={this.portalRefFunc} 
+						node={this.props.optionsContainerRef.current}
+					>
+						<div className="options-box">
+							<div className="options-title">Math Details</div>
+
+							{/*  LaTex Adjustment */}
+							<label className="form-label">
+								LaTeX
+							</label>
 							<textarea
 								placeholder={'Enter LaTeX math'}
 								className={'pt-input pt-fill'}
 								value={this.props.value}
 								onChange={this.handleValueChange}
 							/>
+							
+							{/*  Display Adjustment */}
+							<label className="form-label">
+								Display
+							</label>
+							<div className={'pt-button-group pt-fill'}>
+								<button
+									className={`pt-button pt-icon-align-left ${!this.props.isBlock ? 'pt-active' : ''}`}
+									onClick={this.changeToInline}
+								>
+									Inline
+								</button>
+								<button
+									className={`pt-button pt-icon-align-justify ${this.props.isBlock ? 'pt-active' : ''}`}
+									onClick={this.changeToBlock}
+								>
+									Block
+								</button>
+							</div>
 						</div>
-					}
-				</div>
+					</Portal>
+				}
 			</div>
 
 		);
