aca283062ba9295b428f8ab93ce3379aa3b1a8a4
Travis Rich <travis.s.rich@gmail.com>
2019-07-14 17:34:26 -0400

    update: Allow null on createBranch

--- src/utilities.js
+++ src/utilities.js
@@ -155,13 +155,13 @@ export const createBranch = (baseFirebaseRef, newFirebaseRef, versionNumber) => 
 		.child('changes')
 		.orderByKey()
 		.startAt(String(0))
-		.endAt(String(versionNumber))
+		.endAt(typeof versionNumber === 'number' ? String(versionNumber) : null)
 		.once('value');
 	const getMerges = baseFirebaseRef
 		.child('merges')
 		.orderByKey()
 		.startAt(String(0))
-		.endAt(String(versionNumber))
+		.endAt(typeof versionNumber === 'number' ? String(versionNumber) : null)
 		.once('value');
 	return Promise.all([getChanges, getMerges]).then(([changesSnapshot, mergesSnapshot]) => {
 		const changesSnapshotVal = changesSnapshot.val() || {};
